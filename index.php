adca22a994dfe73b75f4194e599d742c57e0b4ea
sam-bra <144506644+sam-bra@users.noreply.github.com>
2023-09-09 13:43:40 -0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -335,6 +335,29 @@
     </script>
 
 
+<?php
+$counterFile = "counter.txt";
+
+// Verifica se o arquivo existe, se não, cria o arquivo e inicia o contador com zero.
+if (!file_exists($counterFile)) {
+    file_put_contents($counterFile, "0");
+}
+
+// Lê o número atual de visitantes do arquivo.
+$counter = (int)file_get_contents($counterFile);
+
+// Incrementa o contador.
+$counter++;
+
+// Salva o novo valor no arquivo.
+file_put_contents($counterFile, $counter);
+?>
+
+
+    <p>Esta página já foi visitada <?php echo $counter; ?> vezes.</p>
+</body>
+</html>
+    
 </center>
 </body>
 </html>
